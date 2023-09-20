e3df4dc52c817d9e3138310a70f67dae01c3c82a
Marcel <103643206+EinfachMarcel1@users.noreply.github.com>
2023-09-20 13:41:46 +0200

    initial commit

---
+++ html/locales/cs.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Wegwerfen";
+invLocale.useItem = "Benutzen";
+invLocale.giveItem = "Geben";
+invLocale.secondInventoryNotAvailable = "Das zweite Inventar ist nicht verfügbar";
